7eabbbb879123eb2d52fdfcecad66fd01de11077
mifelix-monte <mifelixgodoy@gmail.com>
2023-06-23 22:47:43 -0300

    change from oracle sql to h2 database

--- src/main/java/com/mibookstore/mibookstoreapi/MibookstoreApiApplication.java
+++ src/main/java/com/mibookstore/mibookstoreapi/MibookstoreApiApplication.java
@@ -1,13 +1,8 @@
 package com.mibookstore.mibookstoreapi;
 
-import com.mibookstore.mibookstoreapi.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class MibookstoreApiApplication {
