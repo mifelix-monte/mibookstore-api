b785aaeb60c558d67c486eb9870583bf42ca007a
mifelix-monte <mifelixgodoy@gmail.com>
2023-06-28 13:45:07 -0300

    swagger implementattion

---
+++ src/main/java/com/mibookstore/mibookstoreapi/config/SwaggerConfiguration.java
@@ -0,0 +1,44 @@
+package com.mibookstore.mibookstoreapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicate.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Mi Bookstore API",
+                "API of a online bookstore",
+                "1.0",
+                "Terms of Service",
+                new Contact("Michele Monteiro",
+                        "https://www.linkedin.com/in/michele-monteiro-036750103/",
+                        "mifelixgodoy@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<>()
+        );
+
+        return apiInfo;
+    }
+}
